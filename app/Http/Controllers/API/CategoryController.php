673e2e96154d2d828de745d553f7a1f80447a522
yotmanreyes <yotman.reyes@gmail.com>
2024-10-29 21:38:25 -0400

    chore:add basic ecommerce migrations files, models, and controllers

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function show($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json($category, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->update($request->all());
+        return response()->json($category);
+    }
+
+    public function destroy($id)
+    {
+        Category::destroy($id);
+        return response()->json(null, 204);
+    }
+}
